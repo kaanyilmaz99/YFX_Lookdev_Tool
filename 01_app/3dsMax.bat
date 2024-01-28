e88a1ff79c6c52bbf8d8b388ed25a450c1bac745
Kaan Yilmaz <kaan.yilmaz99otmail.de>
2024-01-28 23:10:16 +0100

    Add new splash screen for max

--- 01_app/3dsMax.bat
+++ 01_app/3dsMax.bat
@@ -12,8 +12,11 @@ set "ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR=%PIPELINEPATH:~0,-1%\dev"
 
 set "MAX_VERSION=2022"
 
+:: --- SPLASH SCREEN ---
+set "SPLASH_IMG=V:\xxPythonAdvanced\Python_Advanced\01_app\dev\img\splash.bmp"
+
 :: --- CALL MAX ---
 set "MAX_DIR=C:/Program Files/Autodesk/3ds Max %MAX_VERSION%"
 set "PATH=%MAX_DIR%"
-start "" "%MAX_DIR%/3dsmax.exe"
+start "" "%MAX_DIR%/3dsmax.exe" -a %SPLASH_IMG%
 
