3716747ae542e8f09b0c7d1f34d3468f629d6e0e
Kaan Yilmaz <kaan.yilmaz99otmail.de>
2024-01-28 21:52:04 +0100

    .bat changes

--- 01_app/3dsMax.bat
+++ 01_app/3dsMax.bat
@@ -4,11 +4,10 @@
 @echo off
 
 :: --- PATH ---
-set "PROJECT_ROOT=../.."
-set "PIPELINEPATH=%PROJECT_ROOT%/dev"
+set "PIPELINEPATH=%~dp0"
 
 :: --- PYTHON ---
-set "PYTHONPATH=%PROJECT_ROOT%/dev"
+set "PYTHONPATH=%PIPELINEPATH:~0,-1%\dev"
 set "ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR=%PYTHONPATH%" 
 
 set "MAX_VERSION=2022"
@@ -17,3 +16,4 @@ set "MAX_VERSION=2022"
 set "MAX_DIR=C:/Program Files/Autodesk/3ds Max %MAX_VERSION%"
 set "PATH=%MAX_DIR%"
 start "" "%MAX_DIR%/3dsmax.exe"
+
