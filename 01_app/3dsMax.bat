4d0690b347d1a9250e5f5b190ca1aa81d6357cea
Kaan Yilmaz <kaan.yilmaz99otmail.de>
2024-01-29 01:01:42 +0100

    Edit splash screen path

--- 01_app/3dsMax.bat
+++ 01_app/3dsMax.bat
@@ -13,7 +13,7 @@ set "ADSK_3DSMAX_STARTUPSCRIPTS_ADDON_DIR=%PIPELINEPATH:~0,-1%\dev"
 set "MAX_VERSION=2022"
 
 :: --- SPLASH SCREEN ---
-set "SPLASH_IMG=V:\xxPythonAdvanced\Python_Advanced\01_app\dev\img\splash.bmp"
+set "SPLASH_IMG=%PYTHONPATH%\img\splash.bmp"
 
 :: --- CALL MAX ---
 set "MAX_DIR=C:/Program Files/Autodesk/3ds Max %MAX_VERSION%"
